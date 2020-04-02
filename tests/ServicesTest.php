09d1c1ec1a16c7a9f5c440264e107c6dd30e25a2
Hernán Cervera <hjcerveram@hotmail.com>
2020-04-01 19:08:00 -0600

    Test READ_groups_GB_major service

---
+++ tests/ServicesTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+
+final class ServicesTest extends TestCase
+{
+    private static $client;
+
+    // Create Guzzle HTTP client for test API calls
+    public static function setUpBeforeClass(): void
+    {
+        $base_uri = 'http://localhost/htdocs_2020/GDHE/src/services/';
+        self::$client = new Client(['base_uri' => $base_uri]);
+    }
+
+    public function test_READ_groups_GB_major()
+    {
+        $service = 'READ_groups_GB_major.php';
+        $response = self::$client->request('GET', $service);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
