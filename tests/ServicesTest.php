2b19c9e1c1219211b9cdbe9bfec8b97c96b9d10b
Hernán Cervera <hjcerveram@hotmail.com>
2020-04-11 11:49:41 -0500

    Implement test_READ_admin_BY_credentials

--- tests/ServicesTest.php
+++ tests/ServicesTest.php
@@ -14,13 +14,30 @@ final class ServicesTest extends TestCase
         self::$client = new Client(['base_uri' => $base_uri]);
     }
 
+    public function test_READ_admin_BY_credentials()
+    {
+        $service = 'READ_admin_BY_credentials.php';
+        $response = self::$client->request('POST', $service, [
+            'json' => [
+                'username' => '0001',
+                'password' => '123'
+            ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getBody(), true);
+
+        $this->assertArrayHasKey('names', $body);
+        $this->assertArrayHasKey('first_lname', $body);
+        $this->assertArrayHasKey('second_lname', $body);
+    }
+
     public function test_READ_groups_GB_major()
     {
         $service = 'READ_groups_GB_major.php';
         $response = self::$client->request('GET', $service);
 
         $this->assertEquals(200, $response->getStatusCode());
-
         $body = json_decode($response->getBody(), true);
 
         if (count($body) > 0) {
