282b6de8970a1d77dd392326e93f13c4837cd890
jefApr <jef.apr.99@gmail.com>
2020-04-11 15:48:15 -0500

    Add icon in the accordion items menu with animation

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
   services.ReadGroups((majors) => {
     majorsList = JSON.parse(majors);
     majorsList.forEach((majorItem) => {
-      item = `<button class='accordion'>${majorItem.major}</button><div class='panel'>`;
+      item = `<button class='accordion'><span><i class="fa fa-angle-right"></i></span>${majorItem.major}</button><div class='panel'>`;
 
       majorItem.groups.forEach((group) => {
-        item += `<button class='subitem' value='${group.group_id}'>${
+        item += `<button class='subitem' value='${group.group_id}'> ${
           group.semester
         } semestre ${group.group_letter ? group.group_letter : ""}`;
         if (!Number(group.approved))
@@ -21,13 +21,16 @@ $(document).ready(function () {
 
   $("#groups").on("click", ".accordion", function () {
     $(this).toggleClass("active");
+    ico = $(this).find(".fa");
     panel = $(this).next();
     panelHeight = panel.css("height");
 
     if (panelHeight.substring(0, panelHeight.length - 2) > 0) {
       panel.css("max-height", "0");
+      ico.css("transform", "rotate(0deg)");
     } else {
       panel.css("max-height", panel.prop("scrollHeight") + "px");
+      ico.css("transform", "rotate(90deg)");
     }
   });
 
