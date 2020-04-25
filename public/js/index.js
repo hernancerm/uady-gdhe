64d935a7e734b29eee245fef0eef6ffbe0d3248d
Hernán Cervera <hjcerveram@hotmail.com>
2020-04-24 19:43:21 -0500

    Display schedule of default selected group on landing

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,12 @@ $(document).ready(function () {
   services.readGroups((majors) => {
     majorsList = JSON.parse(majors);
     idGroupSelected = majorsList[0].groups[0].group_id;
+
+    // Display schedule of default selected group on landing
+    services.readClassesGroupedByWeekday(idGroupSelected, (collegeClasses) => {
+      visualizer.render(JSON.parse(collegeClasses));
+    });
+
     majorsList.forEach((majorItem) => {
       item = `<button class='accordion'><span><i class="fa fa-angle-right"></i></span>${majorItem.major}</button><div class='panel'>`;
       majorItem.groups.forEach((group) => {
