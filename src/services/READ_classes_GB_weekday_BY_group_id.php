0d9f187caa0a19a3ec11955fb3d3a1c4d0f95b7c
Hernán Cervera <hjcerveram@hotmail.com>
2020-04-24 19:22:12 -0500

    Service fix: remove seconds from start and end hours

--- src/services/READ_classes_GB_weekday_BY_group_id.php
+++ src/services/READ_classes_GB_weekday_BY_group_id.php
@@ -21,17 +21,22 @@ $response = array();
 foreach ($weekdays as $weekday) {
     $response[] = [
         'weekday' => $weekday,
-        'classes' => $connection->select('class', [
-            '[><]course' => 'course_id',
-            '[><]subject' => 'subject_id',
-            '[><]classroom' => 'classroom_id',
-        ], [
-            'subject.name(subject_name)', 'class.start_hour', 'class.end_hour', 'classroom.name(classroom)'
-        ], [
-            'course.group_id' => $group_id,
-            'class.weekday' => $weekday,
-            'ORDER' => ['class.start_hour' => 'ASC']
-        ])
+        'classes' => $connection->query(
+            'SELECT 
+                <s.name> AS subject_name,
+                DATE_FORMAT(<c.start_hour>, \'%k:%i\') AS start_hour,
+                DATE_FORMAT(<c.end_hour>, \'%k:%i\') AS end_hour,
+                <cr.name> AS classroom
+            FROM <class> <c>
+                INNER JOIN <course> <co> ON <co.course_id> = <c.course_id>
+                INNER JOIN subject <s> ON <s.subject_id> = <co.subject_id>
+                INNER JOIN <classroom> <cr> ON <cr.classroom_id> = <c.classroom_id>
+            WHERE <co.group_id> = :group_id AND <c.weekday> = :weekday ORDER BY <c.start_hour> ASC;',
+            [
+                ':group_id' => $group_id,
+                ':weekday' => $weekday
+            ]
+        )->fetchAll()
     ];
 }
 
